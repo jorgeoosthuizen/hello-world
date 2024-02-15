4942dbe33ba2352932c64eb0a208df78f1452f29
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-02-15 09:14:21 +0000

    Update App1.js

---
+++ Ficha 1/App1.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+//Ex5
+//IMPLEMENTAÇÃO
+function calculadora(notateorica,notapratica){
+   var media = (notateorica*0.4)+(notapratica*0.6)
+   console.log("A tua média é:" + media)
+
+   if (media<9.5)
+        console.log("Reprovado!")
+   else
+        console.log("Aprovado!") 
+
+
+}
+
+//INVOCAÇÃO
+calculadora(12,10)
+calculadora(9,9)
+
+//Ex6
+
+function getMonth(month){
+    var month=parseInt(prompt("Indique o número do mês:"))
+    switch case()
+}
+
+getMonth()
