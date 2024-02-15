67d6475c866259f2f9b1e0aa9fcd063fd49e5db9
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-02-15 10:53:44 +0000

    Ficha1Completa

--- Ficha 1/App1.js
+++ Ficha 1/App1.js
@@ -2,27 +2,74 @@ console.log("Hello World");
 
 //Ex5
 //IMPLEMENTAÇÃO
-function calculadora(notateorica,notapratica){
-   var media = (notateorica*0.4)+(notapratica*0.6)
-   console.log("A tua média é:" + media)
+function calculadora(notateorica, notapratica) {
+     var media = (notateorica * 0.4) + (notapratica * 0.6)
+     console.log("A tua média é:" + media)
 
-   if (media<9.5)
-        console.log("Reprovado!")
-   else
-        console.log("Aprovado!") 
+     if (media < 9.5)
+          console.log("Reprovado!")
+     else
+          console.log("Aprovado!")
 
 
 }
 
 //INVOCAÇÃO
-calculadora(12,10)
-calculadora(9,9)
+calculadora(12, 10)
+calculadora(9, 9)
 
 //Ex6
 
-function getMonth(month){
-    var month=parseInt(prompt("Indique o número do mês:"))
-    switch case()
+
+
+
+
+function calculator(op1, op2, operator) {
+     if (operator == "+")
+          return op1 + op2;
+     else if (operator == "*")
+          return op1 * op2;
+     else if (operator == "-")
+          return op1 - op2;
+     else if (operator == "/")
+          return op1 / op2;
+     else if (operator == "^")
+          return op1 ** op2;
+     else
+          console.log("Operador errado!")
 }
 
-getMonth()
+var res = calculator(4, 4, "^");
+console.log(res);
+
+
+function multipleOf(value, limit) {
+     for (var i = value; i < limit; i += value)
+          console.log(i)
+}
+
+multipleOf(3, 60)
+
+/*
+function addto(num) {
+     for (var i = 1; i <= num; i++) {
+          soma += i;
+     }
+     return soma;
+}
+
+
+console.log(addto(10));
+
+
+function fact(n) {
+     var f = 1;
+     for (var i = n; i > 1; i--) {
+          f *= i;
+     }
+     return f;
+}
+
+console.log("Factorial de:" + n + "é:" + fact(4))
+
+*/
