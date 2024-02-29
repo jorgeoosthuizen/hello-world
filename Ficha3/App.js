ff5ad3bd800e7352a2c69308fa58a974cd31822e
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-02-29 09:39:12 +0000

    Exercicios 1,2,3

---
+++ Ficha3/App.js
@@ -0,0 +1,22 @@
+function started(){
+    console.log("Started Download...");
+}
+
+function update(progress){
+    console.log(progress + "% of Download");
+}
+
+function completed(){
+    console.log("Download Completed!");
+}
+
+
+function performDownload(started,update,completed){
+    started();
+    for (let i = 0; i <= 100; i++) {
+        update(i);
+    }
+    completed();
+}
+
+performDownload(started,update,completed);
