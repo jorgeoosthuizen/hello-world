d61d74aedc1bfcc1c8c7fe722c9c11290ca34ec4
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-03-14 18:09:01 +0000

    a

--- Ficha5/server.js
+++ Ficha5/server.js
@@ -29,17 +29,69 @@ app.post('/users', (req, res) => {
 });
 
 app.delete('/users/:id', (req, res) => {
-  res.send("DELETE BY ID");
+  var id = req.params.id;
+  for (var i = 0; i < dataObj.data.length; i++) {
+      dataObj.data = dataObj.data.filter((person) => person.id != id);
+      res.send(dataObj.data);
+    }
 });
 
 app.get('/users/:id', (req, res) => {
-  var id = 2;
-  for (var i = 0; i < dataObj.data.length;i++){
-    var person = dataObj.data[i];
-    
+  var id = req.params.id;
+  var person = null;
+  for (var i = 0; i < dataObj.data.length; i++) {
+    if (dataObj.data[i].id == id) {
+      person = dataObj.data[i];
+      break;
+    }
   }
+  if (person == null) {
+    res.send("User not found.")
+  }
+  else {
+    res.send(person);
+  }
+
 });
 
+/*
 app.put('/users/:id', (req, res) => {
-  res.send("PUT");
+  var id = req.params.id;
+  var { newFirstName, newLastName, newProfession, newAge } = req.body; 
+  for (let person of dataObj.data) {
+    if (person.id == id) {
+        person.firstName = newFirstName;
+        person.lastName = newLastName;
+        person.profession = newProfession;
+        person.age = newAge;
+    }
+    else{
+      res.send("User not found!")
+    }
+}
+  res.send("Updated user :" + JSON.stringify(dataObj.data));
 });
+*/
+
+
+app.put('/users/:id', (req, res) => {
+  var id = req.params.id;
+  var details = req.body; 
+  var contains = false;
+  details.id = id;
+  for (i=0;i<dataObj.data.length;i++) {
+    if (dataObj.data[i].id == id) {
+      dataObj.data[i] = details;
+      contains = true;
+      break;
+    }
+}
+
+if(contains == false){
+  res.send("User not found!");
+}
+else{
+  res.send("Updated user :" + details);
+}  
+});
+
