4ce527b76b9c9d7cc76b04fbb80ebab15e01388c
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-03-21 11:23:04 +0000

    ficha6ate9

--- Ficha6/server.js
+++ Ficha6/server.js
@@ -10,12 +10,17 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
-fs.openSync("./log.txt", "w");
+fs.openSync("./log.txt", "a+");
 
-
+function log (req,res){
+  var str = (req.route.path + " -- " + req.method + " -- " + new Date().toUTCString() + "\n");
+  fs.appendFileSync("./log.txt", str)
+}
 
 app.get('/', (req, res) => {
+  log(req,res);
   //const body = 'Hello world';
+  var d = new Date();
   var body = fs.readFileSync('saudacao.html', 'utf-8');
   body = body.replace("current_date", d.toLocaleDateString());
   res.writeHead(200, {
@@ -25,6 +30,54 @@ app.get('/', (req, res) => {
   });
   res.end(body);
 });
+
+
+app.get('/greet/:name', (req, res) => {
+  log(req,res);
+  var name = req.params.name;
+  var d = new Date();
+  var body = fs.readFileSync('saudacao.html', 'utf-8');
+  body = body.replace("current_date", d.toLocaleDateString());
+  body = body.replace("Hello", "Hello " + name + ", today is: ");
+  res.writeHead(200, {
+    'Content-Length': Buffer.byteLength(body),
+    'Content-Type': 'text/html'
+  });
+  res.end(body);
+});
+
+
+app.get('/log.txt', (req,res) => {
+  log(req,res);
+  var str = (req.route.path + "," + req.method + "," + new Date().toUTCString());
+  res.send(str);
+})
+
+app.get('/log', (req,res) => {
+  log(req,res);
+  var log = fs.readFileSync("log.txt" , "utf-8");
+  res.send(log);
+})
+
+app.get('/log/download', (req,res) => {
+  log(req,res);
+  var dFicheiro = 'C:/Users/Jorge Oosthuizen/Documents/GitHub/hello-world/Ficha6/log.txt'
+  res.download(dFicheiro);
+})
+
+app.get('/log/delete', (req,res) => {
+  fs.unlink("C:/Users/Jorge Oosthuizen/Documents/GitHub/hello-world/Ficha6/log.txt", (err)=>{
+    if (err){
+      console.log("Não foi possivel eliminar!");
+      res.send("Não foi possivel eliminar!");
+    }
+    else{
+      console.log("Ficheiro eliminado!");
+      res.send("Ficheiro eliminado!");
+    }
+  })
+  
+})
 
 /*
 
