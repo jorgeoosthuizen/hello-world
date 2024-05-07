faac9a1b90f143f0a8251b13f38b58a07903845a
Jorge Andrade <124082372+jorgeoosthuizen@users.noreply.github.com>
2024-05-07 18:47:48 +0100

    table relations

--- ficha10/sequelize.js
+++ ficha10/sequelize.js
@@ -9,6 +9,13 @@ const User = UserDataModel(sequelize, DataTypes);
 const Book = BookDataModel(sequelize, DataTypes);
 const Loan = LoanDataModel(sequelize, DataTypes);
 
+
+User.hasMany(Loan, {foreignKey: 'user_id'});
+Loan.belongsTo(User, {foreignKey: 'user_id'});
+
+Book.hasMany(Loan, {foreignKey: 'book_id'});
+Loan.belongsTo(Book, {foreignKey: 'book_id'});
+
 sequelize.authenticate()
     .then(() => {
         console.log("Connection has been established");
